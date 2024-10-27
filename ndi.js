fe93615ce62913f7e86f468a3e8f4921ff5706a4
grrrwaaa <grrrwaaa@gmail.com>
2024-10-27 19:24:33 -0400

    removed grandiose

--- ndi.js
+++ ndi.js
@@ -1,63 +1,47 @@
 const path = require("path")
 const { Worker } = require('worker_threads')
 
-
 const { gl, glfw, glutils, Window, Shaderman, Config } = require("../anode_gl/index.js")
+const { Sender, Receiver } = require("../anode_ndi/index.js")
 
-function makeNDItexture(gl, name) {
+function makeNDItexture(gl, name, xres=320, yres=240, fps=25) {
 
-    let metadata = []
-    let status = new Uint32Array(1)
-
-    let worker = new Worker("./ndi_worker.js", { 
-        workerData: { name, status, metadata }
-    })
-
+    let receiver = new Receiver(name, fps)
+    
     let self = {
-        tex: glutils.createTexture(gl, { wrap: gl.CLAMP_TO_BORDER }),
+        xres, yres,
+        tex: glutils.createTexture(gl, { 
+            width: xres, height: yres 
+        }),
         frame: 0,
-        status,
-        metadata,
-        worker,
+        receiver: new Receiver(name, fps=25)
     }
 
-    worker.on("message", msg => {
-        switch(msg.msg) {
-            case "metadata": {
-                self.metadata = msg.metadata
-                break;
-            }
-            case "frame": {
-                self.frame++
-            } break;
-            case "properties": {
-                console.log("ndi_worker properties", msg)
-            
-                // reallocate our texture to match:
-                self.tex.dispose()
-                let tex = glutils.createTexture(gl, { 
-                    channels: 4, // or 2 if msg.kind = UYVY?
-                    width: msg.xres, 
-                    height: msg.yres,
-                    filter: gl.LINEAR
+    setInterval(function() {
+        // it won't wait. If there's no new video frame, it returns null
+        let frame = receiver.video()
+        if (frame) {
+            // frame has xres, yres, bytelength, and data (Uint8Array)
+            if (frame.xres != self.xres || frame.yres != self.yres) {
+                self.xres = frame.xres
+                self.yres = frame.yres
+
+                // recreate texture:
+                self.tex = glutils.createTexture(gl, { 
+                    channels: 4, 
+                    width: self.xres, 
+                    height: self.yres
                 }).allocate()
-            
-                // replace the video texture data with a sharedarraybuffer
-                // so that we can write to it from a worker thread:
-                tex.data = new Uint8Array(new SharedArrayBuffer(tex.data.byteLength))
-            
-                // send this to the worker to write into:
-                worker.postMessage({
-                    data: tex.data
-                })
-
-                self.tex = tex
-                break;
             }
-        }
 
-        
-    })
+            // copy
+            self.tex.data.set(frame.data)
+
+            // notify
+            self.frame++
+        }
+    
+    }, 1000/fps) 
 
     return self
 }
