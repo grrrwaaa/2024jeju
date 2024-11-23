869d6ce072a0c05d12615834786769451205fdc0
grrrwaaa <grrrwaaa@gmail.com>
2024-11-22 19:08:10 -0500

    .\sequence\`

--- run.js
+++ run.js
@@ -10,3 +10,30 @@ child.on('exit', function () {
 });
 
 child.start();
+
+
+// const pm2 = require('pm2')
+
+// pm2.connect(function(err) {
+//   if (err) {
+//     console.error(err);
+//     process.exit(2);
+//   }
+//   pm2.start([
+//     {
+//       script: "render.js",
+//       output: "/dev/stdout",
+//       error: "/dev/stderr",
+//       watch: true,
+        
+//     // Specify delay between watch interval
+//     watch_delay: 1000,
+
+//     },
+//   ]
+//     , function(err, proc) {
+//       if(err) {
+//         throw err
+//       }
+//     });
+// })
--- sequence.js
+++ sequence.js
@@ -10,6 +10,7 @@ return {
         load("ink.js", 0),
         load("descend.js", 60),
         load("blobs.js", 180),
+        load("blobs_wild.js", 250),
         load("spinners.js", 360),
         load("hairy.js", 400),
         load("ascend.js", 480),
