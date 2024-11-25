68089dee90d14a0e094d684a3a54a40ded9ab8af
grrrwaaa <grrrwaaa@gmail.com>
2024-11-24 21:11:30 -0500

    update

--- app.js
+++ app.js
@@ -701,7 +701,8 @@ class App extends Window {
         final_fbo.bind()
 
         if (isFloor) {
-            lidar_fbo.bind()
+            //lidar_fbo.bind()
+            lidar_filter_fbo.bind()
         }
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
