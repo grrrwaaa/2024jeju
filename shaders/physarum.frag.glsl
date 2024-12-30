e83b8c43a267e9e4b65d9198e84f983b623de682
Graham Wakefield <grrrwaaa@gmail.com>
2024-12-30 01:02:41 -0500

    update

--- shaders/physarum.frag.glsl
+++ shaders/physarum.frag.glsl
@@ -320,7 +320,7 @@ void main() {
     
 
     out0.xyz = P.xyz;
-    out0.w = clamp(out0.w, 0., 1.5);
+    out0.w = clamp(out0.w, 0., 2.);
 
     //if (u_init > 0.5 || t < 1) out0 = init(U);
     //if (u_init > 0.5) out0 = vec4(0);
