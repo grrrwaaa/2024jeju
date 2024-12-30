b8b6c45c5815476aacb753593030b5b58552eeeb
Graham Wakefield <grrrwaaa@gmail.com>
2024-12-30 04:02:21 -0500

    update

--- shaders/physarum.frag.glsl
+++ shaders/physarum.frag.glsl
@@ -241,7 +241,7 @@ void main() {
         && oldtrail >= u_spawn_threshold
     ) { //} && oldtrail >= u_spawn_threshold) {
     
-        float u_spawn_mix = 0.5;
+        float u_spawn_mix = 0.9;
         P.xy = mix(P.xy, U, u_spawn_mix);
         P.z = rand(P.xy);
     }
