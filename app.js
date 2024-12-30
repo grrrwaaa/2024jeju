ad9492376a389db98ba871cec0a4eaff7b559ac8
Graham Wakefield <grrrwaaa@gmail.com>
2024-12-29 22:10:06 -0500

    update

--- app.js
+++ app.js
@@ -670,7 +670,7 @@ class App extends Window {
         
         final_fbo.begin()
         {
-            let { width, height } = send_fbo
+            let { width, height } = final_fbo
             gl.viewport(0, 0, width, height);
             gl.clearColor(0, 0, 0, 0)
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
