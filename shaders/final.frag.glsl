677fc52914e5e676e3dee40590e88e96e524453b
Graham Wakefield <grrrwaaa@gmail.com>
2024-12-30 01:34:57 -0500

    update

--- shaders/final.frag.glsl
+++ shaders/final.frag.glsl
@@ -156,7 +156,8 @@ void main() {
         vec3 aura_hsl = vec3(0.4, 0.8, 0.8);
         vec3 aura_hsl_variation = vec3(-0.5, 0., 0.);
         vec3 aura = hsl2rgb(aura_hsl + fluid.w*aura_hsl_variation)*matter*u_final_aura;
-        out0.rgb = max(out0.rgb, aura);
+        if (mod(t, 3) < 1.5)        out0.rgb = max(out0.rgb, aura);
+       else out0.rgb += aura;
     }
 
     // out0.rgb = vec3(sin(t)*0.45+0.55);
