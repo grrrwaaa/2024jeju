ca679d7de3fad9d4aa9fadf46526b67f2dc6ddc2
Graham Wakefield <grrrwaaa@gmail.com>
2024-12-29 22:05:19 -0500

    update

--- app.js
+++ app.js
@@ -712,7 +712,7 @@ class App extends Window {
         }
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);// gl.LINEAR_MIPMAP_LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         // if (this.isFLoor) {
         //     lidar_filter_fbo.bind()
