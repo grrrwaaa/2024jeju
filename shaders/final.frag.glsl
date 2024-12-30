29021f9de3af804d106f3273e7cc0efcf3440d5f
Graham Wakefield <grrrwaaa@gmail.com>
2024-12-29 23:06:22 -0500

    update

--- shaders/final.frag.glsl
+++ shaders/final.frag.glsl
@@ -110,8 +110,8 @@ void main() {
     
 
     // final pass:
-    out0.rgb = adjustSaturation(out0.rgb, u_saturation*2);
-    out0.rgb = pow(out0.rgb, vec3(u_gamma * 0.5));
+    out0.rgb = adjustSaturation(out0.rgb, u_saturation*1.5);
+    out0.rgb = pow(out0.rgb, vec3(u_gamma * 0.7));
 
     //out0.rgb = vec4(fluid.z) * u_final_pressure;
     //out0.rgb = vec3(1);
