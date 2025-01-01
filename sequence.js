f8005b955c6c92a16bd6f755592bee88a75c7f16
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-01 03:07:42 -0500

    local

--- sequence.js
+++ sequence.js
@@ -29,7 +29,7 @@ return {
             // base color of ocean (hue, sat, lightness in 0..1)
             u_ocean_hsl: [0.6, 0.5, 0.55],
             // how this varies by vertical 
-            u_ocean_hsl_variation: [0., 0.5, 0.2],
+            u_ocean_hsl_variation: [0., 0.5, 0.5],
             // base color of aura (hue, sat, lightness in 0..1)
             u_aura_hsl: [0.4, 0.8, 0.5],
             // how this varies by aura intensity:
