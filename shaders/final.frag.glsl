2e58f4c59efcf0ddef0d50909d4d6ec272d1969a
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-01 03:16:08 -0500

    local

--- shaders/final.frag.glsl
+++ shaders/final.frag.glsl
@@ -47,7 +47,7 @@ vec2 dim = textureSize(u_tex_fluid, 0);
 void main() {
     float t = u_seconds;
     float frame = t*60.;
-    bool isFloor = u_wall_u.y < 0.;
+    bool isFloor = v_normal.y > 0.;
 
     vec3 normal = texture(u_tex_normal, v_uv).xyz; //normalize(v_normal);
     vec3 spherical = texture(u_tex_spherical, v_uv).xyz;
@@ -158,7 +158,7 @@ void main() {
         //     out0.rgb = vec3(0);
         // }
 
-        out0.rgb = v_normal;
+        out0.rgb = isFloor ? vec3(1.) : vec3(0.);
 
       
 
