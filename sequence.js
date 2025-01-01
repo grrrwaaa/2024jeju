971a374b67d2282e49c2eced70b6edb183003242
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-01 02:05:00 -0500

    local

--- sequence.js
+++ sequence.js
@@ -531,8 +531,8 @@ return {
             u_final_creatures: 0.75,
         
             // colors:
-            u_creatures_hsl: [0.833, 0.8, 0.88],
-            u_creatures_hsl_inside: [0.955, 0.9, 0.98],
+            u_creatures_hsl: [0.833, 0.8, 0.6777],
+            u_creatures_hsl_inside: [0.955, 0.97, 0.6],
             // base color of ocean (hue, sat, lightness in 0..1)
             u_ocean_hsl: [0.678, 0.5, 0.6],
             // how this varies by vertical 
