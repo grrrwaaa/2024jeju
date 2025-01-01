60247e7e17b275e70525b685bf9b914a7230a631
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-01 03:41:30 -0500

    local

--- sequence.js
+++ sequence.js
@@ -8,8 +8,8 @@ function load(s, time) {
 return {
     _seq: [
         {
-            _name: "ascend",
-            _time: 520, // 8 min 40 seconds Adaptation to home coming
+            _name: "storm!",
+            _time: 580, // 9 min 40 cm, homecoming
         
             // overall contrast -- 1 is no change, >1 is deeper contrast (darker darks), <1 is softer (lighter)
             u_gamma: 1.2,
@@ -20,16 +20,16 @@ return {
             // visual intensities:
             u_final_pressure: 0.5,
             u_final_aura: 0.5,
-            u_final_trails: 0.35,
-            u_final_creatures: 0.5,
+            u_final_trails: 0.3,
+            u_final_creatures: 0.1,
         
             // colors:
-            u_creatures_hsl: [0.62, 0.55, 0.4],
-            u_creatures_hsl_inside: [0.69, 0.88, 0.5],
+            u_creatures_hsl: [0.4333, 0.7, 0.7],
+            u_creatures_hsl_inside: [0.5111, 0.6, 0.6],
             // base color of ocean (hue, sat, lightness in 0..1)
             u_ocean_hsl: [0.6, 0.5, 0.6],
             // how this varies by vertical 
-            u_ocean_hsl_variation: [0., 0.4, 0.2],
+            u_ocean_hsl_variation: [0., 0.2, 0.2],
             // base color of aura (hue, sat, lightness in 0..1)
             u_aura_hsl: [0.4, 0.8, 0.8],
             // how this varies by aura intensity:
@@ -38,17 +38,17 @@ return {
             // behaviours:
         
             // -8 to 8? if we are descending or rising in the ocean. 0 means neither, negative is descending
-            u_descend: 0.5,
-            u_drift_amount: 0.5,
+            u_descend: 0,
+            u_drift_amount: 0.8,
         
             // 0-0.03: amount of sandy dust in the fluid currents
             // setting to 0 will create more glassy smooth surface but also let human currents travel farther; feels more deep oceah
-            u_grain: 0, //0.015,
+            u_grain: 0.005,
             // 0 or 1, changes how the fluid currents travel; subtle effect but I like 1 better than 0
             u_fluid_mode: 1.0,
             // should be 1 or very slightly less than 1. Probably 1 is ok. 
-            u_fluid_pressure_decay: 0.9999,
-            u_fluid_matter_decay: 0.9999,
+            u_fluid_pressure_decay: 0.99999,
+            u_fluid_matter_decay: 0.99999,
             u_fluid_velocity_decay: 1,
         
             // creatures:
@@ -64,32 +64,32 @@ return {
             // how they sense:
             // smaller makes their trails narrower
             // larger (hundreds) makes them more like flocks
-            u_sensor_distance : 25.,
+            u_sensor_distance : 40.,
             // 0..pi. smaller means they can't see behind them, so they  tend to create lines. larger means they can see all directions, more likely to make balls. 
             u_sensor_angle : 3.1,
             // 0 to about 0.3? how much they follow trails, 0 makes them senseless dust, about 0.1-0.2 they organize well, above that it gets messier
             u_turn_angle : 0.01,
             // 0 to about 0.5? how much they randomly wander. 0.2 feels good, 0.5 is messier
-            u_wander_angle : 0.001,
+            u_wander_angle : 0.01,
         
             // how fast they move by different stimuli:
             // too high and they will die off. 
             // 0-6? following the fluid currents (e.g. human)
-            u_fluid_effect_speed : 3,
+            u_fluid_effect_speed : 2,
             // 0..1, drifting with the ocean
-            u_drift_effect_speed : 1.4,
+            u_drift_effect_speed : 1.8,
             // 0..6? speeding up when near a trail. Too high and they die off, too low and they don't feel alive
-            u_trail_effect_speed : 3,
+            u_trail_effect_speed : 2,
         
             // 0..0.2? how strongly they create their trails. too strong and the trails blow up
             u_deposit_rate : 0.2,
             // 0..0.2? how much the trails spread in space
             // increasing this well tend to make them decay more quickly, so increase deposit_rate or decay_rate to compensate?
-            u_blur_rate : 0.7,
+            u_blur_rate : 0.9,
             // 0..1, but usually 0.95+
             // how much their trails decay over time
             // if it is very close to 1 the trails tend to persist as large patterns
-            u_decay_rate : 0.9,
+            u_decay_rate : 0.91,
         },
     ]
 }
