37f3f57343149d2f44cd5223332fa0e5a2137693
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-01 02:27:18 -0500

    local

--- shaders/final.frag.glsl
+++ shaders/final.frag.glsl
@@ -176,7 +176,7 @@ void main() {
         //out0 = vec4(0);
 
         float dots = exp(-0.9*length(gl_FragCoord.xy - physarum.xy));
-        vec3 creature_color = hsl2rgb(0.1*vec3(0, duv) + mix(u_creatures_hsl, u_creatures_hsl_inside, pow(clamp(physarum.w*0.5, 0., 1.), 2.)));
+        vec3 creature_color = hsl2rgb(0.1*vec3(duv, 0) + mix(u_creatures_hsl, u_creatures_hsl_inside, pow(clamp(physarum.w*0.5, 0., 1.), 2.)));
         out0.rgb += creature_color*dots*u_final_creatures * 5.;//(0.7, 1, 0.9, 0,) (0.3, 0.7, 0.5, 0), alien shrimp (0.8, 0.01, 0.01, 0)
 
     }
