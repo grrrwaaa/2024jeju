5f7ef5118a0457d3a9b4405bd37e5b3a455ddc0c
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-01 03:18:07 -0500

    local

--- shaders/final.frag.glsl
+++ shaders/final.frag.glsl
@@ -154,6 +154,11 @@ void main() {
         out0.rgb *= 1. + spherical.y;
         out0.rgb = adjustSaturation(out0.rgb, 1.0 + spherical.y);
 
+        if (isFloor) {
+            out0.rgb = vec3(0);
+        } 
+
+        
       
 
         // vec3 aura_hsl = u_aura_hsl; //vec3(0.4, 0.8, 0.8);
