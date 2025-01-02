749f3b5aecdf2a2b5e13a914948d38941dca2377
Graham Wakefield <grrrwaaa@gmail.com>
2025-01-02 02:39:45 -0500

    updated content res

--- shaders/physarum.frag.glsl
+++ shaders/physarum.frag.glsl
@@ -242,23 +242,23 @@ void main() {
     
     float nearby = 1.;//-exp(-dist);
 
-    if ((
-        (1.-u_caustic_spawn) < caustic*rnd.y*nearby
-        || (1.-u_aura_spawn) < pow(fluid.w, 5)*rnd.x *nearby)
-        && oldtrail >= u_spawn_threshold
-    ) { //} && oldtrail >= u_spawn_threshold) {
+    // if ((
+    //     (1.-u_caustic_spawn) < caustic*rnd.y*nearby
+    //     || (1.-u_aura_spawn) < pow(fluid.w, 5)*rnd.x *nearby)
+    //     && oldtrail >= u_spawn_threshold
+    // ) { //} && oldtrail >= u_spawn_threshold) {
     
-        float u_spawn_mix = 0.1;
-        P.xy = mix(P.xy, U, u_spawn_mix);
-        P.z = rand(P.xy);
-    }
-
-    // // always some random spawning:
-    // if (rnd.y > 0.9999) {
-    //     P.xy = U;
+    //     float u_spawn_mix = 0.1;
+    //     P.xy = mix(P.xy, U, u_spawn_mix);
     //     P.z = rand(P.xy);
-    //     P.w = rand(P.yx);
     // }
+
+    // always some random spawning:
+    if (rnd.y > 0.9999) {
+        P.xy = U;
+        P.z = rand(P.xy);
+        P.w = rand(P.yx);
+    }
 
     //P.xy = mix(P.xy, U, 0.00001*rnd.z*rnd.z);
 
